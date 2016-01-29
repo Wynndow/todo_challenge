6ee00fd7ef73a1093db5efa4902cbb527565418c
Chris Wynne <chris.wynndow@gmail.com>
2016-01-29 17:37:36 +0000

    Adds feature test for adding a task

--- spec/e2e/toDoListFeature.js
+++ spec/e2e/toDoListFeature.js
@@ -1,6 +1,14 @@
 describe('To Do List Feature', function() {
+
   it('has a title', function() {
     browser.get('http://localhost:8080');
     expect(browser.getTitle()).toEqual('To Do List');
+  });
+
+  it('allows you to add tasks via a form and view them', function() {
+    browser.get('http://localhost:8080');
+    element(by.model("toDoCtrl.newTask")).sendKeys('Make a to do list.');
+    element(by.className("button")).click();
+    expect(element(by.tagName('li')).getText()).toEqual('Make a to do list.');
   });
 });
