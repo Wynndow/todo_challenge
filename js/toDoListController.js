02e9fb5dce2e4e7e9ccee846c973d015feff7b00
Chris Wynne <chris.wynndow@gmail.com>
2016-01-29 18:36:19 +0000

    Adds ability to add tasks to list via form

--- js/toDoListController.js
+++ js/toDoListController.js
@@ -1,3 +1,10 @@
 toDoList.controller('ToDoListController', [function() {
 
+
+  var self = this;
+  self.taskList = []
+
+  self.addTask = function() {
+    self.taskList.push({"task": self.newTask});
+  };
 }]);
--- spec/toDoListController.spec.js
+++ spec/toDoListController.spec.js
@@ -8,7 +8,18 @@ describe('toDoListController', function() {
   }));
 
   it('initialises with an empty list of tasks', function() {
-    expect(ctrl.taskList).toBeUndefined();
+    expect(ctrl.taskList.length).toEqual(0);
+  });
+
+  describe('when adding a task', function() {
+
+    var itemOne = "Make a to do list"
+
+    it('retains the task', function() {
+      ctrl.newTask = "Adding a new task"
+      ctrl.addTask();
+      expect(ctrl.taskList[0].task).toEqual("Adding a new task")
+    });
   });
 
 });
