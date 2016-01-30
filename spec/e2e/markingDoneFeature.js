27a8763b7d7b64e13b535ca318b1704796b7e058
Chris Wynne <chris.wynndow@gmail.com>
2016-01-30 16:40:50 +0000

    Adds CSS to strike though completed tasks

--- spec/e2e/markingDoneFeature.js
+++ spec/e2e/markingDoneFeature.js
@@ -8,4 +8,13 @@ describe('Marking a task as done', function() {
     expect(element(by.tagName('span')).getAttribute('class')).toMatch('completed-true')
   });
 
+  it('uncrosses out the task when the done button is clicked again', function() {
+    browser.get('http://localhost:8080');
+    element(by.model("toDoCtrl.newTask")).sendKeys('Make a to do list.');
+    element(by.buttonText("Add")).click();
+    element(by.buttonText("Done?")).click();
+    element(by.buttonText("Done?")).click();
+    expect(element(by.tagName('span')).getAttribute('class')).toMatch('completed-false')
+  });
+
 });
