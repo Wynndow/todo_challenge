b4f3783da517c9a94badda5699faeb57443246c4
Chris Wynne <chris.wynndow@gmail.com>
2016-01-31 13:47:15 +0000

    Updates protractor tests for change of html

--- spec/e2e/editingTasksFeature.js
+++ spec/e2e/editingTasksFeature.js
@@ -7,10 +7,10 @@ describe('Editing a task', function() {
   });
 
   it('allows a task to be edited via form when you click it', function() {
-    element(by.className('taskText')).click();
+    element(by.className('completed-false')).click();
     element(by.model("task.text")).sendKeys(' And profit');
     element(by.buttonText('Save')).click();
-    expect(element(by.className('taskText')).getText()).toEqual('Make a to do list. And profit');
+    expect(element(by.className('completed-false')).getText()).toEqual('Make a to do list. And profit');
   });
 
   it('hides the edit form before the task is clicked', function() {
@@ -20,8 +20,8 @@ describe('Editing a task', function() {
   });
 
   it('hides the task while being edited', function() {
-    element(by.className('taskText')).click();
-    expect(element(by.className('taskText')).isDisplayed()).toBe(false);
+    element(by.className('completed-false')).click();
+    expect(element(by.className('completed-false')).isDisplayed()).toBe(false);
   });
 
 });
